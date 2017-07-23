5a0d74497e9c5e73ae6ed9b88f050fcde0a155a9
Ravikanth C <ravikanth@ravichaganti.com>
2017-07-23 21:59:26 +0530

    removed function def

--- GetCimClassProperties.ps1
+++ GetCimClassProperties.ps1
@@ -1,73 +1,70 @@
-function Get-CimClassProperty
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [String] $ClassName,
+
+    [Parameter(Mandatory)]
+    [String] $Namespace,
+
+    [Parameter()]
+    [String] $PropertyName
+)
+
+function extractProperty($cimProperty)
 {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [String] $ClassName,
-
-        [Parameter(Mandatory)]
-        [String] $Namespace,
-
-        [Parameter()]
-        [String] $PropertyName
-    )
-
-    function extractProperty($cimProperty)
-    {
-        $cimProperty | Select Name, CimType, `
-        @{
-            l='EmbeddedInstanceOf';e={
-                if ($_.Qualifiers.Name -contains 'EmbeddedInstance')
-                {
-                    $_.Qualifiers.Where({$_.Name -eq 'EmbeddedInstance'}).Value
-                }
-            }
-        },
-        @{
-            l='IsReadyOnly';e={
-                $_.Qualifiers.Name -contains 'read'
-            }
-        },
-        @{
-            l='AllowedValues';e={
-                if ($_.Qualifiers.Name -contains 'ValueMap') {
-                    $_.Qualifiers.Where({$_.Name -eq 'ValueMap'}).Value
-                }
-            }
-        },
-        @{
-            l='IsKey';e={
-                $_.Qualifiers.Name -contains 'Key'
+    $cimProperty | Select Name, CimType, `
+    @{
+        l='EmbeddedInstanceOf';e={
+            if ($_.Qualifiers.Name -contains 'EmbeddedInstance')
+            {
+                $_.Qualifiers.Where({$_.Name -eq 'EmbeddedInstance'}).Value
             }
         }
+    },
+    @{
+        l='IsReadyOnly';e={
+            $_.Qualifiers.Name -contains 'read'
+        }
+    },
+    @{
+        l='AllowedValues';e={
+            if ($_.Qualifiers.Name -contains 'ValueMap') {
+                $_.Qualifiers.Where({$_.Name -eq 'ValueMap'}).Value
+            }
+        }
+    },
+    @{
+        l='IsKey';e={
+            $_.Qualifiers.Name -contains 'Key'
+        }
     }
+}
 
-    try
+try
+{
+    $cimClass = Get-CimClass -ClassName $ClassName -Namespace $Namespace -ErrorAction Stop
+
+    if ($PropertyName)
     {
-        $cimClass = Get-CimClass -ClassName $ClassName -Namespace $Namespace -ErrorAction Stop
-
-        if ($PropertyName)
+        $cimProperty = $cimClass.CimClassProperties.Where({ $_.Name -eq $PropertyName })
+        if ($cimProperty)
         {
-            $cimProperty = $cimClass.CimClassProperties.Where({ $_.Name -eq $PropertyName })
-            if ($cimProperty)
-            {
-                extractProperty -cimProperty $cimProperty
-            }
-            else
-            {
-                throw "${PropertyName} does not exist in the CIM Class"   
-            }
+            extractProperty -cimProperty $cimProperty
         }
         else
         {
-            foreach ($property in $cimClass.CimClassProperties)
-            {
-                extractProperty -cimProperty $property
-            }
+            throw "${PropertyName} does not exist in the CIM Class"   
         }
     }
-    catch
+    else
     {
-        Write-Error $_
+        foreach ($property in $cimClass.CimClassProperties)
+        {
+            extractProperty -cimProperty $property
+        }
     }
+}
+catch
+{
+    Write-Error $_
 }
